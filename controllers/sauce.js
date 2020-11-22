35d88cc591c71a179c66b156a99f1a57b8469284
RianneStreef <riannestreef@gmail.com>
2020-11-22 13:44:36 +0100

    problems with POST

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -27,7 +27,7 @@ exports.getOneSauce = (req, res, next) => {
 };
 
 exports.createSauce = (req, res, next) => {
-  req.body.sauce = JSON.parse(req.body.thing);
+  req.body.sauce = JSON.parse(req.body.sauce);
   const url = req.protocol + "://" + req.get("host");
   const sauce = new Sauce({
     _id: req.body._id,
@@ -57,8 +57,51 @@ exports.createSauce = (req, res, next) => {
     });
 };
 
-exports.modifySauce = (req, res, next) => {};
+exports.modifySauce = (req, res, next) => {
+  let sauce = new Sauce({ _id: req.params._id });
+  if (req.file) {
+    const url = req.protocol + "://" + req.get("host");
+    req.body.sauce = JSON.parse(req.body.sauce);
+    sauce = {
+      _id: req.body._id,
+      userId: req.body.userId,
+      name: req.body.name,
+      manufacturer: req.body.manufacturer,
+      description: req.body.description,
+      mainPepper: req.body.mainPepper,
+      imageUrl: url + "/images/" + req.file.filename,
+      heat: req.body.heat,
+      likes: req.body.likes,
+      dislikes: req.body.dislikes,
+      userLikes: req.body.userLikes,
+      userDislikes: req.body.userDislikes,
+    };
+  }
+  Sauce.updateOne({ _id: req.params.id }, sauce)
+    .then(() => {
+      res.status(201).json({
+        message: "Sauce updated successfully!",
+      });
+    })
+    .catch((error) => {
+      res.status(400).json({
+        error: error,
+      });
+    });
+};
 
-exports.deleteSauce = (req, res, next) => {};
+exports.deleteSauce = (req, res, next) => {
+  Sauce.deleteOne({ _id: req.params.id })
+    .then(() => {
+      res.status(200).json({
+        message: "Deleted!",
+      });
+    })
+    .catch((error) => {
+      res.status(400).json({
+        error: error,
+      });
+    });
+};
 
 exports.likeSauce = (req, res, next) => {};
