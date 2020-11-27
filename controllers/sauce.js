cc4bcec84b0aa5e4c1afc7cba74a9f45e75f9acc
Martin Kruger <martin@martinkruger.com>
2020-11-27 10:34:50 +0200

    Code from video

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -1,13 +1,17 @@
-const Sauce = require("../models/sauce");
+const Sauce = require('../models/sauce');
 
 exports.getAllSauces = (req, res, next) => {
+  console.log('getAllSauces');
   Sauce.find()
     .then((sauces) => {
+      console.log('getAllSauces: Success');
+      console.log(sauces);
       res.status(200).json(sauces);
     })
     .catch((error) => {
+      console.log('getAllSauces: Fail');
       res.status(400).json({
-        error: error,
+        error: error + 'Huh',
       });
     });
 };
@@ -27,31 +31,60 @@ exports.getOneSauce = (req, res, next) => {
 };
 
 exports.createSauce = (req, res, next) => {
-  req.body.sauce = JSON.parse(req.body.sauce);
-  const url = req.protocol + "://" + req.get("host");
+  console.log('createSauce: start');
+  // console.log(req);
+  // req.body.sauce = JSON.parse(req.body.sauce);
+  // const parsedSauce = JSON.parse(req.body.sauce);
+  const url = req.protocol + '://' + req.get('host');
+
+  console.log(req.body.sauce);
+  console.log(req.body);
+  const {
+    userId,
+    name,
+    manufacturer,
+    description,
+    mainPepper,
+    heat,
+    // usersDisliked,
+    // usersLiked,
+    // dislikes,
+    // likes,
+  } = JSON.parse(req.body.sauce);
+
+  const imageUrl = url + '/images/' + req.file.filename;
+
+  const likes = 10;
+  const dislikes = 10;
+  const usersLiked = 'Yup';
+  const usersDisliked = 'Nope';
+
   const sauce = new Sauce({
-    // _id: req.body._id,
-    userId: req.body.sauce.userId,
-    name: req.body.sauce.name,
-    manufacturer: req.body.sauce.manufacturer,
-    description: req.body.sauce.description,
-    mainPepper: req.body.sauce.mainPepper,
-    imageUrl: url + "/images/" + req.file.filename,
-    heat: req.body.sauce.heat,
-    // likes: req.body.likes,
-    // dislikes: req.body.dislikes,
-    // userLikes: req.body.userLikes,
-    // userDislikes: req.body.userDislikes,
+    userId,
+    name,
+    manufacturer,
+    description,
+    mainPepper,
+    usersDisliked,
+    usersLiked,
+    dislikes,
+    likes,
+    heat,
+    imageUrl,
   });
+
   console.log(sauce);
   sauce
     .save()
     .then(() => {
+      console.log('createSauce: success');
       res.status(201).json({
-        message: "Post saved successfully!",
+        message: 'Post saved successfully!',
       });
     })
     .catch((error) => {
+      console.log('createSauce: error');
+      console.log(error);
       res.status(400).json({
         error: error,
       });
@@ -61,7 +94,7 @@ exports.createSauce = (req, res, next) => {
 exports.modifySauce = (req, res, next) => {
   let sauce = new Sauce({ _id: req.params._id });
   if (req.file) {
-    const url = req.protocol + "://" + req.get("host");
+    const url = req.protocol + '://' + req.get('host');
     req.body.sauce = JSON.parse(req.body.sauce);
     sauce = {
       _id: req.body._id,
@@ -70,18 +103,18 @@ exports.modifySauce = (req, res, next) => {
       manufacturer: req.body.manufacturer,
       description: req.body.description,
       mainPepper: req.body.mainPepper,
-      imageUrl: url + "/images/" + req.file.filename,
+      imageUrl: url + '/images/' + req.file.filename,
       heat: req.body.heat,
       likes: req.body.likes,
       dislikes: req.body.dislikes,
       userLikes: req.body.userLikes,
-      userDislikes: req.body.userDislikes,
+      userDisliked: req.body.userDisliked,
     };
   }
   Sauce.updateOne({ _id: req.params.id }, sauce)
     .then(() => {
       res.status(201).json({
-        message: "Sauce updated successfully!",
+        message: 'Sauce updated successfully!',
       });
     })
     .catch((error) => {
@@ -95,7 +128,7 @@ exports.deleteSauce = (req, res, next) => {
   Sauce.deleteOne({ _id: req.params.id })
     .then(() => {
       res.status(200).json({
-        message: "Deleted!",
+        message: 'Deleted!',
       });
     })
     .catch((error) => {
--- models/sauce.js
+++ models/sauce.js
@@ -1,6 +1,5 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const sauceSchema = mongoose.Schema({
-  _id: { type: String, required: true },
   userId: { type: String, required: true },
   name: { type: String, required: true },
   manufacturer: { type: String, required: true },
@@ -10,8 +9,8 @@ const sauceSchema = mongoose.Schema({
   heat: { type: Number, required: true },
   likes: { type: Number, required: true },
   dislikes: { type: Number, required: true },
-  usersLikes: { type: String, required: true },
+  usersLiked: { type: String, required: true },
   usersDisliked: { type: String, required: true },
 });
 
-module.exports = mongoose.model("Sauce", sauceSchema);
+module.exports = mongoose.model('Sauce', sauceSchema);
