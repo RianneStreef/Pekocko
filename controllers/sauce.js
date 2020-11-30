54be2a8b4e90b9c6bef0d30c6e4a16b58fc2b25c
Martin Kruger <martin@martinkruger.com>
2020-11-30 14:26:11 +0200

    Session end

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -1,5 +1,5 @@
-const sauce = require("../models/sauce");
-const Sauce = require("../models/sauce");
+const sauce = require('../models/sauce');
+const Sauce = require('../models/sauce');
 
 exports.getAllSauces = (req, res, next) => {
   Sauce.find()
@@ -31,7 +31,7 @@ exports.getOneSauce = (req, res, next) => {
 };
 
 exports.createSauce = (req, res, next) => {
-  const url = req.protocol + "://" + req.get("host");  
+  const url = req.protocol + '://' + req.get('host');
   const {
     userId,
     name,
@@ -41,7 +41,7 @@ exports.createSauce = (req, res, next) => {
     heat,
   } = JSON.parse(req.body.sauce);
 
-  const imageUrl = url + "/images/" + req.file.filename;
+  const imageUrl = url + '/images/' + req.file.filename;
 
   const likes = 0;
   const dislikes = 0;
@@ -65,7 +65,7 @@ exports.createSauce = (req, res, next) => {
     .save()
     .then(() => {
       res.status(201).json({
-        message: "Post saved successfully!",
+        message: 'Post saved successfully!',
       });
     })
     .catch((error) => {
@@ -76,17 +76,26 @@ exports.createSauce = (req, res, next) => {
 };
 
 exports.modifySauce = (req, res, next) => {
-  const url = req.protocol + "://" + req.get("host");  
-  const {
-    name,
-    manufacturer,
-    description,
-    mainPepper,
-    heat,
-  } = JSON.parse(req.body.sauce);
+  console.log('modifySauce RUNNING');
+  const url = req.protocol + '://' + req.get('host');
+  // console.log(req.body.sauce);
+  console.log(req.body);
+  /* const { name, manufacturer, description, mainPepper, heat } = JSON.parse(
+    req.body,
+  ); */
 
-  const imageUrl = url + "/images/" + req.file.filename;
-  
+  const { name, manufacturer, description, mainPepper, heat } = req.body;
+
+  console.log(req);
+  console.log(req.file);
+
+  let imageUrl;
+
+  if (req.hasOwnProperty('file')) {
+    imageUrl = url + '/images/' + req.file.filename;
+  }
+  // const
+
   const sauce = new Sauce({
     name,
     manufacturer,
@@ -98,26 +107,14 @@ exports.modifySauce = (req, res, next) => {
 
   console.log('Modified sauce' + sauce);
 
-  Sauce.findOneAndUpdate({_id: req.params.id}, sauce).then(
-    () => {
-      res.status(201).json({
-        message: 'Sauce updated successfully!'
-      });
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
-};
+  Sauce.name = name;
+  Sauce.manufacturer = manufacturer;
+  Sauce.save();
 
-exports.deleteSauce = (req, res, next) => { 
-  Sauce.deleteOne({ _id: req.params.id })
+  Sauce.findOneAndUpdate({ _id: req.params.id }, sauce)
     .then(() => {
-      res.status(200).json({
-        message: "Deleted!",
+      res.status(201).json({
+        message: 'Sauce updated successfully!',
       });
     })
     .catch((error) => {
@@ -125,69 +122,91 @@ exports.deleteSauce = (req, res, next) => {
         error: error,
       });
     });
-  };
+};
+
+exports.deleteSauce = (req, res, next) => {
+  Sauce.deleteOne({ _id: req.params.id })
+    .then(() => {
+      res.status(200).json({
+        message: 'Deleted!',
+      });
+    })
+    .catch((error) => {
+      res.status(400).json({
+        error: error,
+      });
+    });
+};
 
 exports.likeSauce = (req, res, next) => {
   Sauce.findOneAndUpdate({
     _id: req.params.id,
-  })
+  });
+
+  console.log('LIKE SAUCE');
+  console.log('----------');
+  console.log(req.body.like);
 
   //determine input - 1/ 0/ -1
 
-  if (input = 1) {
-    const alreadyLiked = usersLiked.includes(userId); 
+  // Gets the input
+  const input = req.body.like;
+
+  // Get the sauce ID
+  // Fetch the sauce from the database using the sauce id
+  // Then you can get the usersLiked and usersDisliked arrays
+
+  if (input === 1) {
+    const alreadyLiked = usersLiked.includes(userId);
     const alreadyDisliked = usersDisliked.includes(userId);
     if (alreadyLiked && alreadyDisliked === -1) {
       // sauce.likes +=
       usersLiked.push(userId);
     }
     if (alreadyLiked || alreadyDisliked >= 0) {
-        (error) => {
-          res.status(400).json({
-            error: "You already evaluated this sauce!" 
-          });
-      }
+      (error) => {
+        res.status(400).json({
+          error: 'You already evaluated this sauce!',
+        });
+      };
     }
-  }
-  if (input = -1) {
-    const alreadyLiked = usersLiked.includes(userId); 
+  } else if (input === -1) {
+    const alreadyLiked = usersLiked.includes(userId);
     const alreadyDisliked = usersDisliked.includes(userId);
     if (alreadyLiked && alreadyDisliked === -1) {
       // sauce.disLikes +=
       usersDisliked.push(userId);
     }
     if (alreadyLiked || alreadyDisliked >= 0) {
-        (error) => {
-          res.status(400).json({
-            error: "You already evaluated this sauce!" 
-          });
-      }
+      (error) => {
+        res.status(400).json({
+          error: 'You already evaluated this sauce!',
+        });
+      };
     }
-  }
-  if (input = 0) {
-    const alreadyLiked = usersLiked.includes(userId); 
+  } else if (input === 0) {
+    const alreadyLiked = usersLiked.includes(userId);
     const alreadyDisliked = usersDisliked.includes(userId);
     if (alreadyLiked && alreadyDisliked === -1) {
       (error) => {
         res.status(400).json({
-          error: "You have not evaluated this sauce yet!" 
+          error: 'You have not evaluated this sauce yet!',
         });
-    }
-    if (alreadyLiked >= 0) {
-      //sauce.likes -=
-      //remove userId from array
-    }
-    if (alreadyDisliked >= 0) {
+      };
+      if (alreadyLiked >= 0) {
+        //sauce.likes -=
+        //remove userId from array
+      }
+      if (alreadyDisliked >= 0) {
         //sauce.likes +=
         //remove userId from array
+      }
     }
-    };
-  } 
+  }
 
   // check if object is updated
 
   // POST object
 
-  // catch 
-
-}
+  // catch
+};
